b44245a9c23e1fbf053280943d61d94e98f2be20
Zhiyang Zuo (George) <33331699+zzuo123@users.noreply.github.com>
2025-09-17 18:46:15 -0400

    used lsp_signature instead of autocmd

--- nvim/lua/plugins/autocomp.lua
+++ nvim/lua/plugins/autocomp.lua
@@ -46,4 +46,16 @@ return {
         })
     end,
 },
+{
+  "ray-x/lsp_signature.nvim",
+  event = "InsertEnter",
+  opts = {
+    bind = true,
+    handler_opts = {
+      border = "rounded"
+    }
+  },
+  -- or use config
+  -- config = function(_, opts) require'lsp_signature'.setup({you options}) end
+}
 }
