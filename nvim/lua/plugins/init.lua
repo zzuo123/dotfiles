c08fba98e7fb23c61760fd8ffb406eacff3eda2f
Zhiyang Zuo (George) <33331699+zzuo123@users.noreply.github.com>
2025-09-19 00:41:56 -0400

    fixed indent and default show git line blame

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,14 +1,17 @@
 -- basic QOL stuff at the absolute minimum
 
 return {
-  "folke/which-key.nvim",
-  {"ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...},
-  {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"},
-  {"nvim-mini/mini.pairs", version = false,
-    config = function()
+    "folke/which-key.nvim",
+    {"ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...},
+    {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"},
+    {"nvim-mini/mini.pairs", version = false, config = function()
         require("mini.pairs").setup()
-    end,
-  },
-  "lewis6991/gitsigns.nvim",
-  "tpope/vim-fugitive",
+    end,},
+    {"lewis6991/gitsigns.nvim", config = function()
+        local gitsigns = require("gitsigns")
+        gitsigns.setup({
+            current_line_blame = true,
+        })
+    end,},
+    "tpope/vim-fugitive",
 }
